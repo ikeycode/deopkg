636f285e07176113f2237ed93554eead0f929815
Ikey Doherty <ikey@serpentos.com>
2023-07-16 22:48:37 +0100

    Implement hookup to install our plugin
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

--- source/deopkg/package.d
+++ source/deopkg/package.d
@@ -15,4 +15,32 @@
 
 module deopkg;
 
-static import packagekit.backend;
+@safe:
+
+import packagekit.plugin;
+
+/** 
+ * Hook up the packagekit plugin with our own system
+ *
+ * Returns: A new PackageKit plugin
+ */
+export extern (C) Plugin packagekit_d_plugin_create()
+{
+    return new EopkgPlugin();
+}
+
+/** 
+ * Implement (d)eopkg support for PackageKit
+ */
+public final class EopkgPlugin : Plugin
+{
+    /** 
+     * Construct a new eopkg plugin
+     */
+    this()
+    {
+        super("deopkg", "eopkg support", "Serpent OS Developers", [
+            "application/x-solus-package"
+        ]);
+    }
+}
